51784c2399c12cf915ada8c800e05ffd69e5ec5f
LinsIan <jjes20055114123@gmail.com>
2025-02-15 21:38:36 +0800

    feat: use simple vertex and fragment shader

--- OpenGLPractice/src/Application.cpp
+++ OpenGLPractice/src/Application.cpp
@@ -2,6 +2,48 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);// 第二個參數是shader的數量
+    glCompileShader(id);
+
+	int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* errMsg = (char*)alloca(length * sizeof(char)); // alloca在statck上申請內存，不需要自己刪除
+        glGetShaderInfoLog(id, length, &length, errMsg);
+        std::cout << "Fiaild to compile" << (type == GL_VERTEX_SHADER ? "Vertex" : "Fragment") << "shader!" << std::endl << errMsg << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    // 連結完後可以把shader原始碼刪除
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+};
+
 int main(void)
 {
     GLFWwindow* window;
@@ -43,6 +85,34 @@ int main(void)
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 	glEnableVertexAttribArray(0);
 
+    // shader說明：宣告position，透過id0取得postion Attrib，並宣告成vector4，不足的資料會自動補
+    std::string vertexSahder = 
+        R"(
+        #version 330 core
+        
+        layout(location = 0) in vec4 position; //in代表輸入變數，會抓該頂點id=0的attrib資料
+
+        void main()
+        {
+            gl_Position = position; // gl_Position是內建的，代表頂點的位置
+        }
+        )";
+
+    std::string fragmentSahder =
+        R"(
+        #version 330 core
+        
+        //定義一個輸出變量，然後綁定到framebuffer的第一個顏色緩衝區（也就是最終呈現在螢幕上的主要顏色buffer)
+        layout(location = 0) out vec4 color;
+
+        void main()
+        {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+        )";
+
+    unsigned int shader = CreateShader(vertexSahder, fragmentSahder);
+    glUseProgram(shader);
 
 
     /* Loop until the user closes the window */
@@ -59,6 +129,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
     
     glfwTerminate();
     return 0;
