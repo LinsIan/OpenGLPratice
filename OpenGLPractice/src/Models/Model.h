7aa0f52a9f3c9bfb3176d5fbda7a664e0ef01ca5
LinsIan <jjes20055114123@gmail.com>
2025-04-06 22:00:33 +0800

    fix: rotation calculation error

--- OpenGLPractice/src/Models/Model.h
+++ OpenGLPractice/src/Models/Model.h
@@ -29,9 +29,9 @@ namespace Model
         void UpdateModelMatrix()
         {
             modelMatrix = glm::translate(glm::mat4(1.0f), *translation) *
-                          glm::rotate(glm::mat4(1.0f), radians->x, glm::vec3(1, 0, 0)) *
-                          glm::rotate(glm::mat4(1.0f), radians->y, glm::vec3(0, 1, 0)) *
-                          glm::rotate(glm::mat4(1.0f), radians->z, glm::vec3(0, 0, 1)) *
+                          glm::rotate(glm::mat4(1.0f), glm::radians(radians->x), glm::vec3(1, 0, 0)) *
+                          glm::rotate(glm::mat4(1.0f), glm::radians(radians->y), glm::vec3(0, 1, 0)) *
+                          glm::rotate(glm::mat4(1.0f), glm::radians(radians->z), glm::vec3(0, 0, 1)) *
                           glm::scale(glm::mat4(1.0f), *scale);
         }
 
